ffe1ea197e25fb44dbff83861298771f8002e17c
Naveed Chaudhry <46507768+reboseyekei@users.noreply.github.com>
2021-05-18 10:42:59 -0500

    testing changes for heroku

--- server.js
+++ server.js
@@ -2,29 +2,24 @@ const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
 
-//You can use whatever port you want here
 //process.env.PORT is something a lot of hosting applications use
-//From what I know it should work for you, but if it doesn't you'll have to do some research
-const port = process.env.PORT || 6969;
+//otherwise it will be at localhost:3000 (if you're doing it locally)
+const port = process.env.PORT || 3000;
 
 
 //Creating the http server
 const server = http.createServer(express);
 
-//Creating the socket server through the http server
+server.listen(port, function(){
+  console.log(`Server is listening on ${port}!`)
+})
+
+//Creating the web socket server through the http server
 const wss = new WebSocket.Server({ server })
 
 //websocket functions
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(data) {
-    wss.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(data);
-      }
-    })
-  })
-})
-
-server.listen(port, function(){
-  console.log(`Server is listening on ${port}!`)
+wss.on('connection', (ws) => {
+  //using the ws on the server
+  console.log("Client has connected");
+  ws.on('close', () => console.log("Client has disconnected"));
 })
