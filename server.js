f5821f97632ac5cbfb435ae0c544e328b13fd0d3
Naveed Chaudhry <46507768+reboseyekei@users.noreply.github.com>
2021-05-18 13:07:35 -0500

    testing

--- server.js
+++ server.js
@@ -1,34 +1,14 @@
-const express = require('express');
-const http = require('http');
 const WebSocket = require('ws');
 
-//You can use whatever port you want here
-//process.env.PORT is something a lot of hosting applications use
-//From what I know it should work for you, but if it doesn't you'll have to do some research
-const port = process.env.PORT || 6969;
+const PORT = process.env.PORT || 8080;
 
+const wss = new WebSocket.Server({ port: PORT });
 
-//Creating the http server
-const server = http.createServer(express);
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
 
-//Creating the socket server through the http server
-const ws = new WebSocket.Server({ server })
+  ws.send('something');
+});
 
-//websocket functions
-ws.on('connection', (ws) => {
-  console.log("Client has connected");
-  ws.on('close', () => console.log("Client has disconnected"));
-  ws.on('message', function incoming(data) {
-    ws.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(data);
-      }
-    })
-  })
-})
-
-server.listen(port, function(){
-  console.log(`Server is listening on ${port}!`)
-})
-
-server.on("error", err=>console.log(err));
